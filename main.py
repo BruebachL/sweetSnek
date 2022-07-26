b4416b25c1b31d09b481f1f4aa0dcbb88af5dbda
BruebachL <lukas.bruebach@student.fhws.de>
2022-07-26 22:05:01 +0200

    Start all submodules and introduce test files.

--- main.py
+++ main.py
@@ -28,16 +28,19 @@ if __name__ == '__main__':
         time.sleep(1)
         cwd = '/'.join(sys.argv[0].split('/')[:-1])
         # Start SMB Server
-        threading.Thread(target=Process.call, args=((cwd + '/honey_smb/HoneySMB2/launch.sh'),)).start()
+        smb_thread = threading.Thread(target=Process.call, args=((cwd + '/honey_smb/HoneySMB2/launch.sh'),))
+        smb_thread.daemon = True
+        smb_thread.start()
         # Import down here so logging server doesn't refuse client connection.
         from honey_os.os_obfuscation import OSObfuscation
         import honey_os.template.os_templates.template_list
         # Start NMap Server
-        threading.Thread(OSObfuscation.run(
+        nmap_thread = threading.Thread(OSObfuscation.run(
             template_path="/".join(
                 inspect.getabsfile(inspect.currentframe()).split("/")[:-1]) + "/honey_os/template/os_templates/" +
                           honey_os.template.os_templates.template_list.template_list[
-                              honey_os.template.os_templates.template_list.use_template], server_ip="127.0.0.1")).start()
-        #sys.exit()
+                              honey_os.template.os_templates.template_list.use_template], server_ip="127.0.0.1"))
+        nmap_thread.daemon = True
+        nmap_thread.start()
     finally:
-        print("Done launching logging server...")
+        print("Logging server closing down...")
