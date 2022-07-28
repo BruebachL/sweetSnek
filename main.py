6b13fe434674086174ae968456ab28fe694540a5
BruebachL <lukas.bruebach@student.fhws.de>
2022-07-29 00:26:12 +0200

    Added a cute little banner :)

--- main.py
+++ main.py
@@ -7,6 +7,15 @@ from honey_log.server.logging_server import LoggingServer
 from honey_os.process import Process
 
 
+def print_banner():
+    banner_file = open("banner.txt")
+    lines = banner_file.readlines()
+    for line in lines:
+        print(line, end='')
+    print()
+    banner_file.close()
+
+
 if __name__ == '__main__':
     try:
         parser = argparse.ArgumentParser(description='Submodule logging client.')
@@ -22,18 +31,21 @@ if __name__ == '__main__':
         else:
             port = None
 
+        print_banner()
+
         # Start server and wait a bit to start clients.
         logging_server = LoggingServer(host, port)
         threading.Thread(target=logging_server.listen, args=()).start()
         time.sleep(1)
         cwd = '/'.join(sys.argv[0].split('/')[:-1])
         # Start SMB Server
-        smb_thread = threading.Thread(target=Process.call, args=((cwd + 'honey_smb/HoneySMB2/launch.sh'),))
+        smb_thread = threading.Thread(target=Process.call, args=((cwd + '/honey_smb/HoneySMB2/launch.sh'),))
         smb_thread.daemon = True
         smb_thread.start()
         # Import down here so logging server doesn't refuse client connection.
         from honey_os.os_obfuscation import OSObfuscation
         import honey_os.template.os_templates.template_list
+
         # Start NMap Server
         nmap_thread = threading.Thread(OSObfuscation.run(
             template_path="/".join(
