5acbd6eafe754802813af38218f8eef37f69077c
BruebachL <lukas.bruebach@student.fhws.de>
2022-07-29 04:35:31 +0200

    correct arguments for string join

--- honey_log/event_logger.py
+++ honey_log/event_logger.py
@@ -78,7 +78,7 @@ class EventLogger:
         if city is None:
             city = ""
         event = json.dumps(
-            HoneypotEvent(HoneypotEventDetails("scan", HoneyPotNMapScanEventContent(ip_to_ping, ' '.join(os_details, country, city)))),
+            HoneypotEvent(HoneypotEventDetails("scan", HoneyPotNMapScanEventContent(ip_to_ping, ' '.join([os_details, country, city])))),
             cls=HoneypotEventEncoder, indent=0).replace('\\"', '"').replace('\\n', '\n').replace('}\"', '}').replace(
             '\"{', '{')
 
