9d64069bb3fdef12a8bfcbd7941f9799855612ab
BruebachL <lukas.bruebach@student.fhws.de>
2022-08-09 09:10:04 +0200

    Introduce login page for capturing credentials. Rename iisstart with proper extension.

--- honey_http/http_server.py
+++ honey_http/http_server.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 import subprocess
 
 from honey_log.client.logging_client import LoggingClient
-from honey_log.honeypot_event import HoneyPotHTTPEventContent
+from honey_log.honeypot_event import HoneyPotHTTPEventContent, HoneyPotLoginEventContent
 
 logging_client = LoggingClient("HTTP")
 app = Flask(__name__)
@@ -13,14 +13,14 @@ def start():
     logging_client.report_event("http",
                                 HoneyPotHTTPEventContent(request.remote_addr, request.method, request.path,
                                                          request.headers.get('User-Agent')))
-    return render_template("iisstart.htm")
+    return render_template("iisstart.html")
 
 @app.route("/index", methods=['GET'])
 def index():
     logging_client.report_event("http",
                                 HoneyPotHTTPEventContent(request.remote_addr, request.method, request.path,
                                                          request.headers.get('User-Agent')))
-    return render_template("iisstart.htm")
+    return render_template("iisstart.html")
 
 
 @app.route("/index.html", methods=['GET'])
@@ -28,7 +28,7 @@ def index_html():
     logging_client.report_event("http",
                                 HoneyPotHTTPEventContent(request.remote_addr, request.method, request.path,
                                                          request.headers.get('User-Agent')))
-    return render_template("iisstart.htm")
+    return render_template("iisstart.html")
 
 
 @app.route("/iisstart.html", methods=['GET'])
@@ -36,7 +36,16 @@ def iisstart():
     logging_client.report_event("http",
                                 HoneyPotHTTPEventContent(request.remote_addr, request.method, request.path,
                                                          request.headers.get('User-Agent')))
-    return render_template("iisstart.htm")
+    return render_template("iisstart.html")
+
+
+@app.route('/login.html', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        logging_client.report_event("login", HoneyPotLoginEventContent(request.remote_addr, "HTTP", request.form['username'], request.form['password']))
+        error = 'Invalid Credentials. Please try again.'
+    return render_template('login.html', error=error)
 
 
 if __name__ == "__main__":
