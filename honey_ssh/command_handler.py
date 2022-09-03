e867d9d28a2069c8376c4284fa5430e0b60d0ebd
BruebachL <lukas.bruebach@student.fhws.de>
2022-09-03 22:13:01 +0200

    Fix spelling mistake.

--- honey_ssh/command_handler.py
+++ honey_ssh/command_handler.py
@@ -28,8 +28,8 @@ class CommandHandler:
         self.known_command_strings["cat /proc/cpuinfo | grep name | wc -l"] = "12\r\n"
         self.known_command_strings["cat /proc/cpuinfo | grep name | head -n 1 | awk '{print ,,,,,;}'"] = "AMD Ryzen 5 1600X Six-Core Processor\r\n"
         self.known_command_strings["cat /proc/cpuinfo | grep name | head -n 1 | awk '{print $4,$5,$6,$7,$8,$9;}'"] = "AMD Ryzen 5 1600X Six-Core Processor\r\n"
-        self.known_command_string["free -m | grep Mem | awk '{print $2 ,$3, $4, $5, $6, $7}'"] = "7957 6355 1420 8 181 1373"
-        self.known_command_string["free -m | grep Mem | awk '{print ,,,,,}'"] = "7957 6355 1420 8 181 1373"
+        self.known_command_strings["free -m | grep Mem | awk '{print $2 ,$3, $4, $5, $6, $7}'"] = "7957 6355 1420 8 181 1373"
+        self.known_command_strings["free -m | grep Mem | awk '{print ,,,,,}'"] = "7957 6355 1420 8 181 1373"
 
 
 
