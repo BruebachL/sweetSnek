2321ee390047465efde93498dc33fafc52a67ad4
BruebachL <lukas.bruebach@student.fhws.de>
2022-09-03 22:42:15 +0200

    Read known command strings from file now.

--- honey_ssh/command_handler.py
+++ honey_ssh/command_handler.py
@@ -25,13 +25,17 @@ class CommandHandler:
         self.output_buffer = []
 
     def populate_known_command_string(self):
-        self.known_command_strings["cat /proc/cpuinfo | grep name | wc -l"] = "12\r\n"
-        self.known_command_strings["cat /proc/cpuinfo | grep name | head -n 1 | awk '{print ,,,,,;}'"] = "AMD Ryzen 5 1600X Six-Core Processor\r\n"
-        self.known_command_strings["cat /proc/cpuinfo | grep name | head -n 1 | awk '{print $4,$5,$6,$7,$8,$9;}'"] = "AMD Ryzen 5 1600X Six-Core Processor\r\n"
-        self.known_command_strings["free -m | grep Mem | awk '{print $2 ,$3, $4, $5, $6, $7}'"] = "7957 6355 1420 8 181 1373\r\n"
-        self.known_command_strings["free -m | grep Mem | awk '{print  ,, , , , }'"] = "7957 6355 1420 8 181 1373\r\n"
-
-
+        from os import listdir
+        from os.path import isfile, join
+        onlyfiles = [join("./commands/", f) for f in listdir("./commands") if isfile(join("./commands/", f))]
+        for filename in onlyfiles:
+            try:
+                with open(filename) as file:
+                    lines = [line.strip('\n').replace('\\n', '\n').replace('\\r', '\r') for line in file.readlines()]
+                    self.known_command_strings[lines[0].strip('\n')] = lines[1:]
+            except Exception as e:
+                import traceback
+                traceback.print_exc(e)
 
     def handle_commands(self):
         writemessage = self.channel.makefile("w")
@@ -42,7 +46,7 @@ class CommandHandler:
 
     def handle_known_command_string(self, writemessage):
         if self.commands in self.known_command_strings:
-            writemessage.write(self.known_command_strings[self.commands])
+            writemessage.write(self.known_command_strings[self.commands][1])
         else:
             return False
         writemessage.channel.send_exit_status(0)
