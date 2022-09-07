8ee1d2a796c3062d90f8d4fd3ac192a598af40e9
BruebachL <lukas.bruebach@student.fhws.de>
2022-09-07 21:46:53 +0200

    Respond to w.

---
+++ honey_ssh/special_commands/query_logged_in_users.py
@@ -0,0 +1,14 @@
+import random
+from datetime import datetime
+
+
+def special_command(args):
+    command_string, client_info, logging_client = pop_default_args(args)
+    return datetime.now().strftime("%H:%M:%S") + " up " + str(
+        random.randrange(10, 59)) + " min,  0 users,  load average: 0." + str(random.randrange(10, 99)) + ", 0." + str(
+        random.randrange(10, 99)) + ", 0." + + str(random.randrange(10, 99)) + "\r\n" + \
+           "USER     TTY      FROM             LOGIN@   IDLE   JCPU   PCPU WHAT" + "\r\n"
+
+
+def pop_default_args(args):
+    return args[0], args[1], args[2]
