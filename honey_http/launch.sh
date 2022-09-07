7396d8f05ccc4d9e086e62edda63f822329964b6
BruebachL <lukas.bruebach@student.fhws.de>
2022-09-07 14:30:27 +0200

    Just call waitress-serve directly in launch script.

--- honey_http/launch.sh
+++ honey_http/launch.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-source env/bin/activate
-waitress-serve --port=80 --ident Microsoft-IIS/8.5 honey_http.http_server:app
+env/bin/waitress-serve --port=80 --ident Microsoft-IIS/8.5 honey_http.http_server:app
