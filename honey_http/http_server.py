050aaac04b3a2008df6484e1de8ef0bd5eeb2fce
BruebachL <lukas.bruebach@student.fhws.de>
2022-09-10 15:25:02 +0200

    Allow upload of files in HTTP submodule.

--- honey_http/http_server.py
+++ honey_http/http_server.py
@@ -1,8 +1,14 @@
+import hashlib
+import os
+from datetime import datetime
+
 from flask import Flask, render_template, request
 import subprocess
 
+from werkzeug.utils import secure_filename
+
 from honey_log.client.logging_client import LoggingClient
-from honey_log.honeypot_event import HoneyPotHTTPEventContent, HoneyPotLoginEventContent
+from honey_log.honeypot_event import HoneyPotHTTPEventContent, HoneyPotLoginEventContent, HoneyPotFileEventContent
 
 logging_client = LoggingClient("HTTP")
 app = Flask(__name__)
@@ -14,6 +20,7 @@ def start():
                                 HoneyPotHTTPEventContent(request.remote_addr, request.method, request.path,
                                                          request.headers.get('User-Agent')))
     return render_template("iisstart.html")
+
 
 @app.route("/index", methods=['GET'])
 def index():
@@ -46,7 +53,9 @@ def login_html():
                                                          request.headers.get('User-Agent')))
     error = None
     if request.method == 'POST':
-        logging_client.report_event("login", HoneyPotLoginEventContent(request.remote_addr, "HTTP", request.form['username'], request.form['password']))
+        logging_client.report_event("login",
+                                    HoneyPotLoginEventContent(request.remote_addr, "HTTP", request.form['username'],
+                                                              request.form['password']))
         error = 'Invalid Credentials. Please try again.'
     return render_template('login.html', error=error)
 
@@ -58,9 +67,35 @@ def login():
                                                          request.headers.get('User-Agent')))
     error = None
     if request.method == 'POST':
-        logging_client.report_event("login", HoneyPotLoginEventContent(request.remote_addr, "HTTP", request.form['username'], request.form['password']))
+        logging_client.report_event("login",
+                                    HoneyPotLoginEventContent(request.remote_addr, "HTTP", request.form['username'],
+                                                              request.form['password']))
         error = 'Invalid Credentials. Please try again.'
     return render_template('login.html', error=error)
+
+
+@app.route('/upload.php', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            return render_template('upload.html')
+        file = request.files['file']
+        if file:
+            filename = secure_filename(file.filename)
+            joined_name = "/tmp/malware/" + '/'.join(filename.split('/')[:-1]) + "/" + datetime.now().strftime("%d-%m-%Y-%H-%M-%S-%f") + "@" + filename.split('/')[-1] + "@" + request.remote_addr
+            file.save(joined_name)
+            with open(joined_name).read() as downloaded_file:
+                file_sha1 = hashlib.sha1(downloaded_file)
+                file_md5 = hashlib.md5(downloaded_file)
+                file_sha256 = hashlib.sha256(downloaded_file)
+                logging_client.report_event("file",
+                                            HoneyPotFileEventContent(request.remote_addr, "HTTP", filename,
+                                                                     file_md5.hexdigest(), file_sha1.hexdigest(),
+                                                                     file_sha256.hexdigest(), len(downloaded_file)))
+
+            return render_template('iisstart.html')
+    return render_template('upload.html')
 
 
 if __name__ == "__main__":
