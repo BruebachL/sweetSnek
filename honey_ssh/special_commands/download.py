8be65da0ea207c3990c5bbf388f47fa0d6cbdb02
BruebachL <lukas.bruebach@student.fhws.de>
2022-09-16 14:43:03 +0200

    Attempt to recursively download additional files from dropper shell scripts.

--- honey_ssh/special_commands/download.py
+++ honey_ssh/special_commands/download.py
@@ -3,6 +3,11 @@ from datetime import datetime
 import requests
 
 from honey_log.honeypot_event import HoneyPotFileEventContent
+from honey_ssh.command_handler import split_command
+
+
+def pop_default_args(args):
+    return args[0], args[1], args[2]
 
 
 def special_command(args):
@@ -22,17 +27,26 @@ def special_command(args):
         address = command_string.split(' ')[-1]  # Take the last and hope for the best
     downloaded_file = requests.get(address).content
     try:
-        with open('/tmp/malware/' + datetime.now().strftime("%d-%m-%Y-%H-%M-%S-%f") + "@" + command_string.split('/')[-1] + "@" + client_info.ip, 'wb') as saved_file:
+        filename = '/tmp/malware/' + datetime.now().strftime("%d-%m-%Y-%H-%M-%S-%f") + "@" + command_string.split('/')[-1] + "@" + client_info.ip
+        with open(filename, 'wb') as saved_file:
             file_sha1 = hashlib.sha1(downloaded_file)
             file_md5 = hashlib.md5(downloaded_file)
             file_sha256 = hashlib.sha256(downloaded_file)
             logging_client.report_event("file", HoneyPotFileEventContent(client_info.ip, "SSH", command_string.split('/')[-1], file_md5.hexdigest(), file_sha1.hexdigest(), file_sha256.hexdigest(), len(downloaded_file)))
             saved_file.write(downloaded_file)
+        if ".sh" in command_string.split('/')[-1]:
+            download_files_from_dropper(filename, client_info, logging_client)
     except Exception as e:
         import traceback
         traceback.print_exc()
     return "\r\n"
 
 
-def pop_default_args(args):
-    return args[0], args[1], args[2]
+def download_files_from_dropper(filename, client_info, logging_client):
+    with open(filename, 'r') as dropper:
+        lines = dropper.readlines()
+        for line in lines:
+            split_commands = split_command(line)
+            for command in split_commands:
+                if "wget" in command or "curl" in command:
+                    special_command((command.replace("wget ", '').replace('curl ', ''), client_info, logging_client))
